042a60eb019a1f91b6d1489474d8804dcdaef24f
chojrakpp <patryk.poniatowski@hotmail.com>
2022-10-01 14:29:36 +0200

    Add project files.

---
+++ Oefening6/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace Oefening6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tb_subt = new System.Windows.Forms.TextBox();
+            this.tb_grandt = new System.Windows.Forms.TextBox();
+            this.tb_ship = new System.Windows.Forms.TextBox();
+            this.tb_salest = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.tb_item_4 = new System.Windows.Forms.TextBox();
+            this.tb_item_2 = new System.Windows.Forms.TextBox();
+            this.tb_item_3 = new System.Windows.Forms.TextBox();
+            this.tb_pricee_4 = new System.Windows.Forms.TextBox();
+            this.tb_pricee_2 = new System.Windows.Forms.TextBox();
+            this.tb_pricee_3 = new System.Windows.Forms.TextBox();
+            this.tb_price_4 = new System.Windows.Forms.TextBox();
+            this.tb_price_2 = new System.Windows.Forms.TextBox();
+            this.tb_price_3 = new System.Windows.Forms.TextBox();
+            this.tb_price_1 = new System.Windows.Forms.TextBox();
+            this.tb_pricee_1 = new System.Windows.Forms.TextBox();
+            this.tb_item_1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_quantity_1 = new System.Windows.Forms.TextBox();
+            this.tb_quantity_2 = new System.Windows.Forms.TextBox();
+            this.tb_quantity_3 = new System.Windows.Forms.TextBox();
+            this.tb_quantity_4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(441, 335);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 15);
+            this.label9.TabIndex = 57;
+            this.label9.Text = "Grand Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(455, 306);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 15);
+            this.label8.TabIndex = 56;
+            this.label8.Text = "Shipping";
+            // 
+            // tb_subt
+            // 
+            this.tb_subt.Location = new System.Drawing.Point(514, 237);
+            this.tb_subt.Name = "tb_subt";
+            this.tb_subt.ReadOnly = true;
+            this.tb_subt.Size = new System.Drawing.Size(100, 23);
+            this.tb_subt.TabIndex = 55;
+            // 
+            // tb_grandt
+            // 
+            this.tb_grandt.Location = new System.Drawing.Point(514, 327);
+            this.tb_grandt.Name = "tb_grandt";
+            this.tb_grandt.ReadOnly = true;
+            this.tb_grandt.Size = new System.Drawing.Size(100, 23);
+            this.tb_grandt.TabIndex = 54;
+            // 
+            // tb_ship
+            // 
+            this.tb_ship.Location = new System.Drawing.Point(514, 298);
+            this.tb_ship.Name = "tb_ship";
+            this.tb_ship.ReadOnly = true;
+            this.tb_ship.Size = new System.Drawing.Size(100, 23);
+            this.tb_ship.TabIndex = 53;
+            // 
+            // tb_salest
+            // 
+            this.tb_salest.Location = new System.Drawing.Point(514, 269);
+            this.tb_salest.Name = "tb_salest";
+            this.tb_salest.ReadOnly = true;
+            this.tb_salest.Size = new System.Drawing.Size(100, 23);
+            this.tb_salest.TabIndex = 52;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(457, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "Sales Tax";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(457, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 15);
+            this.label5.TabIndex = 48;
+            this.label5.Text = "Subtotal";
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(263, 304);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(75, 23);
+            this.btn_calc.TabIndex = 47;
+            this.btn_calc.Text = "Calculate";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // tb_item_4
+            // 
+            this.tb_item_4.Location = new System.Drawing.Point(186, 179);
+            this.tb_item_4.Name = "tb_item_4";
+            this.tb_item_4.Size = new System.Drawing.Size(100, 23);
+            this.tb_item_4.TabIndex = 46;
+            // 
+            // tb_item_2
+            // 
+            this.tb_item_2.Location = new System.Drawing.Point(186, 121);
+            this.tb_item_2.Name = "tb_item_2";
+            this.tb_item_2.Size = new System.Drawing.Size(100, 23);
+            this.tb_item_2.TabIndex = 45;
+            // 
+            // tb_item_3
+            // 
+            this.tb_item_3.Location = new System.Drawing.Point(186, 150);
+            this.tb_item_3.Name = "tb_item_3";
+            this.tb_item_3.Size = new System.Drawing.Size(100, 23);
+            this.tb_item_3.TabIndex = 44;
+            // 
+            // tb_pricee_4
+            // 
+            this.tb_pricee_4.Location = new System.Drawing.Point(298, 179);
+            this.tb_pricee_4.Name = "tb_pricee_4";
+            this.tb_pricee_4.Size = new System.Drawing.Size(100, 23);
+            this.tb_pricee_4.TabIndex = 43;
+            // 
+            // tb_pricee_2
+            // 
+            this.tb_pricee_2.Location = new System.Drawing.Point(298, 121);
+            this.tb_pricee_2.Name = "tb_pricee_2";
+            this.tb_pricee_2.Size = new System.Drawing.Size(100, 23);
+            this.tb_pricee_2.TabIndex = 42;
+            // 
+            // tb_pricee_3
+            // 
+            this.tb_pricee_3.Location = new System.Drawing.Point(298, 150);
+            this.tb_pricee_3.Name = "tb_pricee_3";
+            this.tb_pricee_3.Size = new System.Drawing.Size(100, 23);
+            this.tb_pricee_3.TabIndex = 41;
+            // 
+            // tb_price_4
+            // 
+            this.tb_price_4.Location = new System.Drawing.Point(514, 180);
+            this.tb_price_4.Name = "tb_price_4";
+            this.tb_price_4.ReadOnly = true;
+            this.tb_price_4.Size = new System.Drawing.Size(100, 23);
+            this.tb_price_4.TabIndex = 40;
+            // 
+            // tb_price_2
+            // 
+            this.tb_price_2.Location = new System.Drawing.Point(514, 122);
+            this.tb_price_2.Name = "tb_price_2";
+            this.tb_price_2.ReadOnly = true;
+            this.tb_price_2.Size = new System.Drawing.Size(100, 23);
+            this.tb_price_2.TabIndex = 39;
+            // 
+            // tb_price_3
+            // 
+            this.tb_price_3.Location = new System.Drawing.Point(514, 151);
+            this.tb_price_3.Name = "tb_price_3";
+            this.tb_price_3.ReadOnly = true;
+            this.tb_price_3.Size = new System.Drawing.Size(100, 23);
+            this.tb_price_3.TabIndex = 38;
+            // 
+            // tb_price_1
+            // 
+            this.tb_price_1.Location = new System.Drawing.Point(514, 93);
+            this.tb_price_1.Name = "tb_price_1";
+            this.tb_price_1.ReadOnly = true;
+            this.tb_price_1.Size = new System.Drawing.Size(100, 23);
+            this.tb_price_1.TabIndex = 37;
+            // 
+            // tb_pricee_1
+            // 
+            this.tb_pricee_1.Location = new System.Drawing.Point(298, 92);
+            this.tb_pricee_1.Name = "tb_pricee_1";
+            this.tb_pricee_1.Size = new System.Drawing.Size(100, 23);
+            this.tb_pricee_1.TabIndex = 36;
+            // 
+            // tb_item_1
+            // 
+            this.tb_item_1.Location = new System.Drawing.Point(186, 92);
+            this.tb_item_1.Name = "tb_item_1";
+            this.tb_item_1.Size = new System.Drawing.Size(100, 23);
+            this.tb_item_1.TabIndex = 35;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(409, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Price Each";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(514, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Item";
+            // 
+            // tb_quantity_1
+            // 
+            this.tb_quantity_1.Location = new System.Drawing.Point(404, 93);
+            this.tb_quantity_1.Name = "tb_quantity_1";
+            this.tb_quantity_1.Size = new System.Drawing.Size(100, 23);
+            this.tb_quantity_1.TabIndex = 58;
+            // 
+            // tb_quantity_2
+            // 
+            this.tb_quantity_2.Location = new System.Drawing.Point(404, 122);
+            this.tb_quantity_2.Name = "tb_quantity_2";
+            this.tb_quantity_2.Size = new System.Drawing.Size(100, 23);
+            this.tb_quantity_2.TabIndex = 59;
+            // 
+            // tb_quantity_3
+            // 
+            this.tb_quantity_3.Location = new System.Drawing.Point(404, 151);
+            this.tb_quantity_3.Name = "tb_quantity_3";
+            this.tb_quantity_3.Size = new System.Drawing.Size(100, 23);
+            this.tb_quantity_3.TabIndex = 60;
+            // 
+            // tb_quantity_4
+            // 
+            this.tb_quantity_4.Location = new System.Drawing.Point(404, 179);
+            this.tb_quantity_4.Name = "tb_quantity_4";
+            this.tb_quantity_4.Size = new System.Drawing.Size(100, 23);
+            this.tb_quantity_4.TabIndex = 61;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_quantity_4);
+            this.Controls.Add(this.tb_quantity_3);
+            this.Controls.Add(this.tb_quantity_2);
+            this.Controls.Add(this.tb_quantity_1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tb_subt);
+            this.Controls.Add(this.tb_grandt);
+            this.Controls.Add(this.tb_ship);
+            this.Controls.Add(this.tb_salest);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.tb_item_4);
+            this.Controls.Add(this.tb_item_2);
+            this.Controls.Add(this.tb_item_3);
+            this.Controls.Add(this.tb_pricee_4);
+            this.Controls.Add(this.tb_pricee_2);
+            this.Controls.Add(this.tb_pricee_3);
+            this.Controls.Add(this.tb_price_4);
+            this.Controls.Add(this.tb_price_2);
+            this.Controls.Add(this.tb_price_3);
+            this.Controls.Add(this.tb_price_1);
+            this.Controls.Add(this.tb_pricee_1);
+            this.Controls.Add(this.tb_item_1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "OrderForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label9;
+        private Label label8;
+        private TextBox tb_subt;
+        private TextBox tb_grandt;
+        private TextBox tb_ship;
+        private TextBox tb_salest;
+        private Label label6;
+        private Label label5;
+        private Button btn_calc;
+        private TextBox tb_item_4;
+        private TextBox tb_item_2;
+        private TextBox tb_item_3;
+        private TextBox tb_pricee_4;
+        private TextBox tb_pricee_2;
+        private TextBox tb_pricee_3;
+        private TextBox tb_price_4;
+        private TextBox tb_price_2;
+        private TextBox tb_price_3;
+        private TextBox tb_price_1;
+        private TextBox tb_pricee_1;
+        private TextBox tb_item_1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox tb_quantity_1;
+        private TextBox tb_quantity_2;
+        private TextBox tb_quantity_3;
+        private TextBox tb_quantity_4;
+    }
+}
